ce89c5f753abd6a9dc25b4397c8cdb195c72fc8a
Chirag Gulati <chirag.gulati282@gmail.com>
2025-04-21 15:33:58 +0530

    port issue fix

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ app.get("/callback", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
